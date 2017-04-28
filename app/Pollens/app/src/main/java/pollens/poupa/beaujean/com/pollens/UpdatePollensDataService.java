94b745c085b5a46a73843f7c3eea464aa1722b87
fabsgc <fabienbeaudimi@hotmail.fr>
2017-04-29 00:06:17 +0200

    service scheduler

--- app/Pollens/app/src/main/java/pollens/poupa/beaujean/com/pollens/UpdatePollensDataService.java
+++ app/Pollens/app/src/main/java/pollens/poupa/beaujean/com/pollens/UpdatePollensDataService.java
@@ -1,6 +1,8 @@
 package pollens.poupa.beaujean.com.pollens;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Intent;
 
 /**
@@ -15,6 +17,17 @@ public class UpdatePollensDataService extends IntentService {
     protected void onHandleIntent(Intent workIntent) {
         DatabaseHelper databaseHelper = DatabaseHelper.getInstance(getApplicationContext());
         FeedDB feedDB = new FeedDB(this);
+        feedDB.loadDepartments();
     }
 
+    @Override
+    public void onDestroy() {
+        // I want to restart this service again in one hour
+        AlarmManager alarm = (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarm.set(
+                alarm.RTC_WAKEUP,
+                System.currentTimeMillis() + (1000 * 60 * 60 * 24),
+                PendingIntent.getService(this, 0, new Intent(this, UpdatePollensDataService.class), 0)
+        );
+    }
 }
